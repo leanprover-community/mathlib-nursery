0479b31fa5b4d39f41e89b8584c9f5bf5271e8ec
Simon Hudon <simon.hudon@gmail.com>
2019-10-26 02:07:06 -0400

    fix test suite

--- test/data/serial.lean
+++ test/data/serial.lean
@@ -36,32 +36,32 @@ structure my_struct :=
 (bounded : xs.length ≤ x)
 
 @[derive [serial, decidable_eq]]
-inductive tree (α : Type)
-| leaf {} : tree
-| node2 : α → tree → tree → tree
-| node3 : α → tree → tree → tree → tree
+inductive tree' (α : Type)
+| leaf {} : tree'
+| node2 : α → tree' → tree' → tree'
+| node3 : α → tree' → tree' → tree' → tree'
 
-open tree
+open tree'
 
-meta def tree.repr {α} [has_repr α] : tree α → string
+meta def tree'.repr {α} [has_repr α] : tree' α → string
 | leaf := "leaf"
-| (node2 x t₀ t₁) := to_string $ format!"(node2 {repr x} {tree.repr t₀} {tree.repr t₁})"
-| (node3 x t₀ t₁ t₂) := to_string $ format!"(node3 {repr x} {tree.repr t₀} {tree.repr t₁} {tree.repr t₂})"
+| (node2 x t₀ t₁) := to_string $ format!"(node2 {repr x} {tree'.repr t₀} {tree'.repr t₁})"
+| (node3 x t₀ t₁ t₂) := to_string $ format!"(node3 {repr x} {tree'.repr t₀} {tree'.repr t₁} {tree'.repr t₂})"
 
-meta instance {α} [has_repr α] : has_repr (tree α) := ⟨ tree.repr ⟩
+meta instance {α} [has_repr α] : has_repr (tree' α) := ⟨ tree'.repr ⟩
 
 def x := node2 2 (node3 77777777777777 leaf leaf (node2 1 leaf leaf)) leaf
 
 #eval serialize x
 -- [17, 1, 5, 2, 430029026, 72437, 0, 0, 1, 3, 0, 0, 0]
-#eval deserialize (tree ℕ) [17, 1, 5, 2, 430029026, 72437, 0, 0, 1, 3, 0, 0, 0]
+#eval deserialize (tree' ℕ) [17, 1, 5, 2, 430029026, 72437, 0, 0, 1, 3, 0, 0, 0]
 -- (some (node2 2 (node3 77777777777777 leaf leaf (node2 1 leaf leaf)) leaf))
 #eval (deserialize _ (serialize x) = some x : bool)
 -- tt
 
 open medium
 
-example (x : tree ℕ) : deserialize _ (serialize x) = some x :=
+example (x : tree' ℕ) : deserialize _ (serialize x) = some x :=
 by { dsimp [serialize,deserialize],
      rw [eval_eval,serial.correctness],
      refl }
